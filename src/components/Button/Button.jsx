3e04347ec6f542bd24655889478eea6d9bd99168
Arina <terekhinaarina00@gmail.com>
2024-11-12 11:05:21 +0500

    remove unnecessary folders

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import classNames from 'classnames';
-
-import styles from './Button.module.css';
-
-export const Button = ({ className, variant = 'default', children, ...props }) => {
-  return (
-    <button className={classNames(styles.button, styles[variant], className)} {...props}>{children}</button>
-  )
-}
--- src/components/Input/Input.jsx
+++
@@ -1,6 +0,0 @@
-import styles from './Input.module.css';
-import React, { forwardRef } from "react";
-
-export const Input = forwardRef((props, ref) => {
-  return <input ref={ref} className={styles.input} {...props} />
-})
